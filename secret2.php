40ac9cc4768d6a6afaa7a2e8a4d14d9d7dcb122e
G1n1o <rafal.szkudlarek88@gmail.com>
2024-03-27 13:29:45 +0100

    endOfProject

--- secret2.php
+++ secret2.php
@@ -5,7 +5,7 @@ include __DIR__ . '/src/Framework/Database.php';
 use Framework\Database;
 
 $db = new Database('mysql', [
-    'host' => 'localhosti',
+    'host' => 'localhost',
     'port' => 3306,
     'dbname' => 'phpiggy'
 ], 'root', '');
--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,16 +2,31 @@
 
 declare(strict_types=1);
 
-use Framework\{TemplateEngine, Database};
+use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService, TransactionService, ReceiptService};
 
 return [
-    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
-    ValidatorService::class => fn () => new ValidatorService(),
-    Database::class => new Database($_ENV['DB_DRIVER'], [
-        'host' => $_ENV['DB_HOST'],
-        'port' => $_ENV['DB_PORT'],
-        'dbname' => $_ENV['DB_NAME']
-    ], $_ENV['DB_USER'], $_ENV['DB_PASS'])
+  TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
+  ValidatorService::class => fn () => new ValidatorService(),
+  Database::class => fn () => new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+  ], $_ENV['DB_USER'], $_ENV['DB_PASS']),
+  UserService::class => function (Container $container) {
+    $db =  $container->get(Database::class);
+
+    return new UserService($db);
+  },
+  TransactionService::class => function (Container $container) {
+    $db =  $container->get(Database::class);
+
+    return new TransactionService($db);
+  },
+  ReceiptService::class => function (Container $container) {
+    $db =  $container->get(Database::class);
+
+    return new ReceiptService($db);
+  }
 ];
